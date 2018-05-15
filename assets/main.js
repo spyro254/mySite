59dad0dce2fec84b77494af11a33dd672d43e75d
spyro254 <36949845+spyro254@users.noreply.github.com>
2018-05-15 13:21:46 +0100

    Updated
    Updated with correct files

--- assets/main.js
+++
@@ -1,9 +0,0 @@
-$(document).ready(function() {
-  $(window).scroll(function() {
-    if ($(document).scrollTop() > 200) {
-      $(".my-name").addClass("test");
-    } else {
-      $(".my-name").removeClass("test");
-    }
-  });
-});
